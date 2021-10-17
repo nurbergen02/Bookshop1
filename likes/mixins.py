bb3bb8e78528538a2b5864f343a0fe2bb8b8042a
user@user-VivoBook-ASUSLaptop-X515JA-X515JA <kanumetovnurbergen@gmail.com>
2021-10-17 19:35:37 +0600

    my self

--- likes/mixins.py
+++ likes/mixins.py
@@ -1,34 +1,34 @@
 # from rest_framework.decorators import action
 # from rest_framework.response import Response
 # from . import services
-# from .serializers import FanSerializer
-#
-#
-# class LikedMixin:
-#     @action(detail=True, methods=['POST'])
-#     def like(self, request, pk=None):
-#         """
-#         Лайкает `obj`.
-#         """
-#         obj = self.get_object()
-#         services.add_like(obj, request.user)
-#         return Response('Вы успешно поставили лайк')
-#
-#     @action(detail=True, methods=['POST'])
-#     def unlike(self, request, pk=None):
-#         """
-#         Удаляет лайк с `obj`.
-#         """
-#         obj = self.get_object()
-#         services.remove_like(obj, request.user)
-#         return Response('Вы удалили лайк')
-#
-#     @action(detail=True, methods=['GET'])
-#     def fans(self, request, pk=None):
-#         """
-#         Получает всех пользователей, которые лайкнули `obj`.
-#         """
-#         obj = self.get_object()
-#         fans = services.get_fans(obj)
-#         serializer = FanSerializer(fans, many=True)
-#         return Response(serializer.data)
+from .serializers import FanSerializer
+
+
+class LikedMixin:
+    @action(detail=True, methods=['POST'])
+    def like(self, request, pk=None):
+        """
+        Лайкает `obj`.
+        """
+        obj = self.get_object()
+        services.add_like(obj, request.user)
+        return Response('Вы успешно поставили лайк')
+
+    @action(detail=True, methods=['POST'])
+    def unlike(self, request, pk=None):
+        """
+        Удаляет лайк с `obj`.
+        """
+        obj = self.get_object()
+        services.remove_like(obj, request.user)
+        return Response('Вы удалили лайк')
+
+    @action(detail=True, methods=['GET'])
+    def fans(self, request, pk=None):
+        """
+        Получает всех пользователей, которые лайкнули `obj`.
+        """
+        obj = self.get_object()
+        fans = services.get_fans(obj)
+        serializer = FanSerializer(fans, many=True)
+        return Response(serializer.data)
--- likes/services.py
+++ likes/services.py
@@ -1,3 +1,46 @@
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+from .models import Like
+
+User = get_user_model()
+
+
+def add_like(obj, user):
+    """Лайкает `obj`.
+    """
+    obj_type = ContentType.objects.get_for_model(obj)
+    likes, is_created = Like.objects.get_or_create(
+        content_type=obj_type, object_id=obj.id, user=user)
+    return likes
+
+
+def remove_like(obj, user):
+    """Удаляет лайк с `obj`.
+    """
+    obj_type = ContentType.objects.get_for_model(obj)
+    Like.objects.filter(
+        content_type=obj_type, object_id=obj.id, user=user
+    ).delete()
+
+
+def is_fan(obj, user) -> bool:
+    """Проверяет, лайкнул ли `user` `obj`.
+    """
+    if not user.is_authenticated:
+        return False
+    obj_type = ContentType.objects.get_for_model(obj)
+    likes = Like.objects.filter(
+        content_type=obj_type, object_id=obj.id, user=user)
+    return likes.exists()
+
+
+def get_fans(obj):
+    """Получает всех пользователей, которые лайкнули `obj`.
+    """
+    obj_type = ContentType.objects.get_for_model(obj)
+    return User.objects.filter(
+        likes__content_type=obj_type, likes__object_id=obj.id)
+
 # from django.contrib.auth import get_user_model
 # from django.contrib.contenttypes.models import ContentType
 # from .models import Like
@@ -6,7 +49,8 @@
 #
 #
 # def add_like(obj, user):
-#     """Лайкает `obj`.
+#     """
+#     Лайкает `obj`.
 #     """
 #     obj_type = ContentType.objects.get_for_model(obj)
 #     likes, is_created = Like.objects.get_or_create(
@@ -15,7 +59,8 @@
 #
 #
 # def remove_like(obj, user):
-#     """Удаляет лайк с `obj`.
+#     """
+#     Удаляет лайк с `obj`.
 #     """
 #     obj_type = ContentType.objects.get_for_model(obj)
 #     Like.objects.filter(
@@ -24,7 +69,8 @@
 #
 #
 # def is_fan(obj, user) -> bool:
-#     """Проверяет, лайкнул ли `user` `obj`.
+#     """
+#     Проверяет, лайкнул ли `user` `obj`.
 #     """
 #     if not user.is_authenticated:
 #         return False
@@ -35,55 +81,9 @@
 #
 #
 # def get_fans(obj):
-#     """Получает всех пользователей, которые лайкнули `obj`.
+#     """
+#     Получает всех пользователей, которые лайкнули `obj`.
 #     """
 #     obj_type = ContentType.objects.get_for_model(obj)
 #     return User.objects.filter(
 #         likes__content_type=obj_type, likes__object_id=obj.id)
-#
-# # from django.contrib.auth import get_user_model
-# # from django.contrib.contenttypes.models import ContentType
-# # from .models import Like
-# #
-# # User = get_user_model()
-# #
-# #
-# # def add_like(obj, user):
-# #     """
-# #     Лайкает `obj`.
-# #     """
-# #     obj_type = ContentType.objects.get_for_model(obj)
-# #     likes, is_created = Like.objects.get_or_create(
-# #         content_type=obj_type, object_id=obj.id, user=user)
-# #     return likes
-# #
-# #
-# # def remove_like(obj, user):
-# #     """
-# #     Удаляет лайк с `obj`.
-# #     """
-# #     obj_type = ContentType.objects.get_for_model(obj)
-# #     Like.objects.filter(
-# #         content_type=obj_type, object_id=obj.id, user=user
-# #     ).delete()
-# #
-# #
-# # def is_fan(obj, user) -> bool:
-# #     """
-# #     Проверяет, лайкнул ли `user` `obj`.
-# #     """
-# #     if not user.is_authenticated:
-# #         return False
-# #     obj_type = ContentType.objects.get_for_model(obj)
-# #     likes = Like.objects.filter(
-# #         content_type=obj_type, object_id=obj.id, user=user)
-# #     return likes.exists()
-# #
-# #
-# # def get_fans(obj):
-# #     """
-# #     Получает всех пользователей, которые лайкнули `obj`.
-# #     """
-# #     obj_type = ContentType.objects.get_for_model(obj)
-# #     return User.objects.filter(
-# #         likes__content_type=obj_type, likes__object_id=obj.id)
